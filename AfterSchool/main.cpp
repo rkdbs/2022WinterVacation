591a3fe4fde68a5d9a2bf750e70f3e023d7f56dd
gayoon <w2207@e-mirim.hs.kr>
2022-12-26 15:33:48 +0900

    적 출현위치 밸런스 조정

--- AfterSchool/main.cpp
+++ AfterSchool/main.cpp
@@ -25,7 +25,7 @@ int main(void) {
 		enemy[i].setSize(Vector2f(70, 70));
 		enemy[i].setFillColor(Color::Yellow);
 		enemy_life[i] = 1;
-		enemy[i].setPosition(rand()%640, rand()%480);
+		enemy[i].setPosition(rand()%300+300, rand()%480); // 300~599
 	}
 
 	// 윈도우가 열려있을 때까지 반복
@@ -72,6 +72,5 @@ int main(void) {
 
 		window.display();
 	}
-
 	return 0;
 }
