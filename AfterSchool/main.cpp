04dfe4ba4ecf9763d334e9ebd151b35c8015f13a
gayoon <w2207@e-mirim.hs.kr>
2022-12-26 13:53:41 +0900

    네모 그리기

--- AfterSchool/main.cpp
+++ AfterSchool/main.cpp
@@ -8,6 +8,10 @@ int main(void) {
 	// 윈도우창 생성
 	RenderWindow window(VideoMode(640, 480), "AfterSchool");
 
+	RectangleShape player;
+	player.setSize(Vector2f(40, 40));
+	player.setPosition(100, 100); // 100, 100 기준으로 40 40 네모 그리기
+
 	// 윈도우가 열려있을 때까지 반복
 	while (window.isOpen()) {
 		Event event;
@@ -18,6 +22,8 @@ int main(void) {
 					window.close(); // 윈도우 닫기
 			}
 		}
+		window.draw(player);
+		window.display();
 	}
 
 	return 0;
