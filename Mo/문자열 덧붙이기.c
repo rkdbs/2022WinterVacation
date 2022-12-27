5d09a0aa4e7b56a05349f9f549cb4fecd209e7b0
gayoon <w2207@e-mirim.hs.kr>
2022-12-27 10:48:42 +0900

    [문법] 문자열 덧붙이기 3

--- Mo/문자열 덧붙이기.c
+++ Mo/문자열 덧붙이기.c
@@ -3,16 +3,9 @@
 
 int main(void) {
 	char str[30] = "score : ";
-	// str의 글자 수 구하기
-	int str_len = strlen(str);
-
 	char str2[30] = "300";
-	// str2의 글자 수 구하기
-	int str2_len = strlen(str2);
 
-	for (int i = 0; i < str2_len; i++)
-		str[i + str_len] = str2[i];
-	str[str_len + str2_len] = '\0'; // 문자의 끝엔 항상 null 값
+	strcatx(str, str2); // str에 str2 추가
 	printf("%s", str);
 
 	int score = 300;
