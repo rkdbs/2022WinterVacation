c6b1aadaa1a8de37d0d58824ba86ad91ecb66b4a
gayoon <w2207@e-mirim.hs.kr>
2022-12-27 11:18:55 +0900

    [문법] 문자열 덧붙이기 5

--- Mo/문자열 덧붙이기.c
+++ Mo/문자열 덧붙이기.c
@@ -3,14 +3,10 @@
 #include <stdlib.h> // itoa 사용
 
 int main(void) {
-	char str[30] = "score : ";
+	char str[30];
 	int score = 300;
+	sprintf(str, "score : %d", score);
 
-	char str2[30] = "300";
-	// 정수를 문자열로 (반환할 정수, 적용대상, 진수)
-	itoa(score, str2, 10);
-
-	strcatx(str, str2); // str에 str2 추가
 	printf("%s", str);
 
 	return 0;
