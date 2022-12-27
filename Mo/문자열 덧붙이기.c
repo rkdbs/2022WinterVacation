93aae74603b9fe3f6cf91c25c208e2342d97511b
gayoon <w2207@e-mirim.hs.kr>
2022-12-27 10:44:47 +0900

    [문법] 문자열 덧붙이기 2

--- Mo/문자열 덧붙이기.c
+++ Mo/문자열 덧붙이기.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
+#include <string.h> // 문자열 라이브러리
 
 int main(void) {
 	char str[30] = "score : ";
 	// str의 글자 수 구하기
-	int str_len = 0;
-	while (str[str_len] != '\0')
-		str_len++;
+	int str_len = strlen(str);
 
 	char str2[30] = "300";
 	// str2의 글자 수 구하기
-	int str2_len = 0;
-	while(str[str_len] != '\0')
-		str2_len++;
+	int str2_len = strlen(str2);
 
 	for (int i = 0; i < str2_len; i++)
 		str[i + str_len] = str2[i];
