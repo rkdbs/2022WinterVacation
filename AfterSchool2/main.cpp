19c3adf24923072005eeb8da1522744dd9d842a1
gayoon <w2207@e-mirim.hs.kr>
2022-12-30 14:38:37 +0900

    카드 위치 크기 설정

--- AfterSchool2/main.cpp
+++ AfterSchool2/main.cpp
@@ -6,7 +6,17 @@
 
 using namespace sf;
 
+const int S = 4;
+
+struct Card {
+	RectangleShape sprite;
+	int id;
+};
+
 int main(void) {
+	int CARD_W = 200;
+	int CARD_H = 200;
+
 	RenderWindow window(VideoMode(1200, 800), "AfterSchool2");
 	window.setFramerateLimit(60);
 
@@ -22,6 +32,15 @@ int main(void) {
 	text.setFillColor(Color::White);
 	text.setPosition(0, 0);
 	char info[40];
+
+	struct Card cards[5][5];
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < S; j++) {
+			cards[i][j].sprite.setSize(Vector2f(CARD_W, CARD_H));
+			cards[i][j].sprite.setPosition(j * CARD_W, i * CARD_H);
+			cards[i][j].sprite.setFillColor(Color(i * 120, j * 120, 0));
+		}
+	}
 
 	while (window.isOpen()) {
 		mouse_pos = Mouse::getPosition(window);
@@ -44,6 +63,11 @@ int main(void) {
 		text.setString(info);
 
 		window.clear(Color::Black);
+		for (int i = 0; i < S; i++) {
+			for (int j = 0; j < S; j++) {
+				window.draw(cards[i][j].sprite);
+			}
+		}
 
 		window.draw(text);
 
