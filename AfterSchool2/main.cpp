392ab1c3b867df33eadfeb8551bbfd7dcef80172
gayoon <w2207@e-mirim.hs.kr>
2023-01-02 11:04:09 +0900

    뒤집혀진 카드가 2개를 넘지 못하도록 차단

--- AfterSchool2/main.cpp
+++ AfterSchool2/main.cpp
@@ -83,15 +83,17 @@ int main(void) {
 				if (event.mouseButton.button == Mouse::Left) {
 					for (int i = 0; i < S; i++) {
 						for (int j = 0; j < S; j++) {
-							// 마우스 위치가 cards[i][j]의 위치에 해당한다면?
-							if (cards[i][j].sprite.getGlobalBounds().contains(mouse_pos.x, mouse_pos.y)) {
-								// 뒤집혀지지 않은 카드만 뒤집겠다.
-								if (cards[i][j].is_clicked == 0) {
-									cards[i][j].is_clicked = 1;
-									flipped_num++;
-									// 두 개를 뒤집었다면
-									if (flipped_num == 2) {
-										delay_time = spent_time;
+							if (flipped_num < 2) {
+								// 마우스 위치가 cards[i][j]의 위치에 해당한다면?
+								if (cards[i][j].sprite.getGlobalBounds().contains(mouse_pos.x, mouse_pos.y)) {
+									// 뒤집혀지지 않은 카드만 뒤집겠다.
+									if (cards[i][j].is_clicked == 0) {
+										cards[i][j].is_clicked = 1;
+										flipped_num++;
+										// 두 개를 뒤집었다면
+										if (flipped_num == 2) {
+											delay_time = spent_time;
+										}
 									}
 								}
 							}
