db94e0649d7f777548202015942317112cde970a
gayoon <w2207@e-mirim.hs.kr>
2023-01-02 11:48:18 +0900

    [문법] swap이 잘 되는 이유

--- Mo/swap.c
+++ Mo/swap.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-// _a와 a, _b와 b는 전혀 상관없는 변수
-void swap(int _a, int _b) {
+// _a와 a, _b와 b는 전혀 상관없는 변수, 따라서 a, b값은 바뀌지 않음.
+void swap(int* _a, int* _b) {
 	int temp;
-	temp = _a;
-	_a = _b;
-	_b = temp;
+	temp = *_a;
+	*_a = *_b;
+	*_b = temp;
 }
 
 int main(void) {
@@ -16,7 +16,7 @@ int main(void) {
 
 	// a와 b를 swap하는 함수를 호출
 
-	swap(a, b);
+	swap(&a, &b); // _a에게 a의 주소값을, _b에게 b의 주소값을 넘겨준다
 
 	printf("a:%d b:%d\n", a, b);
 }
