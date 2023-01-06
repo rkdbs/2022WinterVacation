f9f1df73b04f8added0084de77c95edf7fdb4661
gayoon <w2207@e-mirim.hs.kr>
2023-01-06 13:51:06 +0900

    UPDATE

--- mysql_ex/main.c
+++ mysql_ex/main.c
@@ -15,14 +15,23 @@ int main(void) {
 
 	int query_result;
 	// INSERT
-	char insert_q[50] = "INSERT INTO korea VALUES('TOKYO', 10000)";
+	//char insert_q[50] = "INSERT INTO korea VALUES('TOKYO', 10000)";
+	//// Query 실행
+	//query_result = mysql_query(connection, insert_q);
+	//if (query_result == 0)
+	//	printf("%s 성공\n", insert_q);
+	//else
+	//	printf("%s 실패\n", insert_q);
 
+	// UPDATE
+	char update_q[100] = "UPDATE korea SET population = 10000 WHERE city = 'TOKYO'";
 	// Query 실행
-	query_result = mysql_query(connection, insert_q);
+	query_result = mysql_query(connection, update_q);
+
 	if (query_result == 0)
-		printf("%s 성공\n", insert_q);
+		printf("%s 성공\n", update_q);
 	else
-		printf("%s 실패\n", insert_q);
+		printf("%s 실패\n", update_q);
 
 	mysql_close(&mysql); // mysql 종료
 }
