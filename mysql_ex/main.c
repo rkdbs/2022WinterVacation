e006efe296e02accf7bbecedcfb1224b21e0d516
gayoon <w2207@e-mirim.hs.kr>
2023-01-06 13:44:03 +0900

    INSERT

--- mysql_ex/main.c
+++ mysql_ex/main.c
@@ -9,9 +9,20 @@ int main(void) {
 	MYSQL* connection = mysql_real_connect(&mysql, DB_HOST, DB_USER, DB_PASS, DB_NAME, 3306, NULL, 0);
 
 	if (connection == NULL)
-		printf("DB 연결 실패");
+		printf("DB 연결 실패\n");
 	else
-		printf("DB 연결 성공");
+		printf("DB 연결 성공\n");
+
+	int query_result;
+	// INSERT
+	char insert_q[50] = "INSERT INTO korea VALUES('TOKYO', 10000)";
+
+	// Query 실행
+	query_result = mysql_query(connection, insert_q);
+	if (query_result == 0)
+		printf("%s 성공\n", insert_q);
+	else
+		printf("%s 실패\n", insert_q);
 
 	mysql_close(&mysql); // mysql 종료
 }
