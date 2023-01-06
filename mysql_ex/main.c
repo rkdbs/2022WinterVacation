0d8a68cfbc45b71b6aa03e9f16515f8b139cc687
gayoon <w2207@e-mirim.hs.kr>
2023-01-06 14:27:48 +0900

    SELECT

--- mysql_ex/main.c
+++ mysql_ex/main.c
@@ -16,6 +16,7 @@ int main(void) {
 	int query_result;
 	// INSERT
 	//char insert_q[50] = "INSERT INTO korea VALUES('TOKYO', 10000)";
+	// 
 	//// Query 실행
 	//query_result = mysql_query(connection, insert_q);
 	//if (query_result == 0)
@@ -25,6 +26,7 @@ int main(void) {
 
 	// UPDATE
 	//char update_q[100] = "UPDATE korea SET population = 10000 WHERE city = 'TOKYO'";
+	// 
 	//// Query 실행
 	//query_result = mysql_query(connection, update_q);
 
@@ -34,14 +36,41 @@ int main(void) {
 	//	printf("%s 실패\n", update_q);
 
 	// DELETE
-	char delete_q[100] = "DELETE FROM korea WHERE city = 'TOKYO'";
-	// Query 실행
-	query_result = mysql_query(connection, delete_q);
+	//char delete_q[100] = "DELETE FROM korea WHERE city = 'TOKYO'";
+	
+	//// Query 실행
+	//query_result = mysql_query(connection, delete_q);
 
+	//if (query_result == 0)
+	//	printf("%s 성공\n", delete_q);
+	//else
+	//	printf("%s 실패\n", delete_q);
+
+	// SELECT
+	char select_q[100] = "SELECT * FROM korea";
+
+	// Query 실행
+	query_result = mysql_query(connection, select_q);
 	if (query_result == 0)
-		printf("%s 성공\n", delete_q);
+		printf("%s 성공\n", select_q);
 	else
-		printf("%s 실패\n", delete_q);
+		printf("%s 실패\n", select_q);
+
+	MYSQL_RES* select_result;
+	select_result = mysql_store_result(connection);
+	MYSQL_ROW sql_row;
+	
+	while (TRUE) {
+		// 각 행(data)을 sql_row가 받는다
+		sql_row = mysql_fetch_row(select_result);
+
+		// 더 이상 받을 행이 없으면
+		if (sql_row == NULL)
+			break;
+		
+		// DB의 int형 데이터도 문자열로 받는다(sql_row[1])
+		printf("%s %s\n", sql_row[0], sql_row[1]);
+	}
 
 	mysql_close(&mysql); // mysql 종료
 }
